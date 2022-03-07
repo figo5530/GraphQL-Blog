fc9a10ffbabe875d4fdc9267220a7b0d88d698e4
figo5530 <figooooo0221@gmail.com>
2022-03-07 14:30:51 -0500

    creates four new components in components folder and brings into index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import { PostCard, Categories, PostWidget } from '../components'
 const post = [
   { title: 'React Testing', excerpt: 'Learn react testing' },
   { title: 'React with Tailwind', excerpt: 'Learn react with TailwindCss' },
@@ -14,16 +14,12 @@ const Home = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {post.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {post.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
-            <div className='lg-sticky relative top-8'>
-
+            <div className='lg:sticky relative top-8'>
+              <PostWidget />
+              <Categories />
             </div>
         </div>
       </div>
