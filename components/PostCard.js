58970b1568e461dc1d062bf21840552709ab732d
figo5530 <figooooo0221@gmail.com>
2022-03-08 14:30:55 -0500

    fixes the bug to properly render the data

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,14 +2,16 @@ import React from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
-function PostCard({post}) {
+function PostCard({ post }) {
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
-
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="object-top absolute h-80 w-full object-cover shadow-lg ronded-t-lg lg:rounded-lg"
+        />
       </div>
-        {post.title}
-        {post.excerpt}
     </div>
   )
 }
