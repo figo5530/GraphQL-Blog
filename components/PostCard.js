d0c51c7ee785af78ea3aaaff4d23c5690887946b
figo5530 <figooooo0221@gmail.com>
2022-03-11 22:10:13 -0500

    adds button of continue reading and fixes some bugs

--- components/PostCard.js
+++ components/PostCard.js
@@ -9,7 +9,7 @@ function PostCard({ post }) {
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-top absolute h-80 w-full object-cover shadow-lg ronded-t-lg lg:rounded-lg"
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
       </div>
       <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
@@ -37,10 +37,14 @@ function PostCard({ post }) {
           </span>
         </div>
       </div>
-      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerrpt}</p>
-      {/* <div className='text-center'>
-
-      </div> */}
+      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
+      <div className='text-center'>
+        <Link href={`/post/${post.slug}`}>
+          <span className='transition duration-500 transform hover:translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+            Continue Reading
+          </span>
+        </Link>
+      </div>
     </div>
   )
 }
