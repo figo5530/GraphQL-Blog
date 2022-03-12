690264de3761035ed858f5a87a42710c9956aeff
figo5530 <figooooo0221@gmail.com>
2022-03-11 21:57:57 -0500

    adds author name and createdAt and excerpt info

--- components/PostCard.js
+++ components/PostCard.js
@@ -19,15 +19,28 @@ function PostCard({ post }) {
       </h1>
       <div className='bloc lg:flex text-center items-center justify-center mb-8 w-full'>
         <div className='flex items-center justify-center mb-4 w-full lg:mb-0 lg:w-auto mr-8'>
-          <img 
+          <img
             alt={post.author.name}
             height="30px"
             width="30px"
             className='align-middle rounded-full'
             src={post.author.photo.url}
           />
+          <p className='inline align-midlle text-gray-700 ml-2 text-lg'>{post.author.name}</p>
+        </div>
+        <div className='font-medium text-gray-700'>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          </svg>
+          <span>
+            {moment(post.createdAt).format("MMM DD, YYYY")}
+          </span>
         </div>
       </div>
+      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerrpt}</p>
+      {/* <div className='text-center'>
+
+      </div> */}
     </div>
   )
 }
