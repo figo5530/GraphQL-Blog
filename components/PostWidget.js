7f7fa2e5242673cad45734d7b9031857d5f04b28
figo5530 <figooooo0221@gmail.com>
2022-03-12 21:47:39 -0500

    adds gql statements for getting the recentpost and similarpost

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
 
-function PostWidget() {
+function PostWidget({ categories, slug }) {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(category, slug).then(res => {
+        setRelatedPosts(res)
+      })
+    } else {
+      getRecentPosts().then(res => {
+        setRelatedPosts(res)
+      })
+    }
+  }, [slug])
+
+  console.log(relatedPosts)
+  
   return (
     <div>PostWidget</div>
   )
